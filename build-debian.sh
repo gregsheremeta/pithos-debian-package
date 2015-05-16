bd482086a6523df151c0eeab1d40ee55d976909d
Greg Sheremeta <greg@gregsheremeta.com>
2015-05-15 21:05:49 -0400

    revamped build process to use pybuild.

--- build-debian.sh
+++ build-debian.sh
@@ -1,47 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 
+VERSION=1.0.2
+DISTRO=utopic
+PKG_VERSION=2ppa2
 
 BUILD_DIR="build"
 NAME="pithos"
-VERSION=1.0.2
-
+SOURCE_URL_BASE="https://github.com/pithos/pithos/archive/"
+DATE=`date -R`
 
 rm -rf $BUILD_DIR
-rm -rf pithos.egg-info
-
-python3 setup.py egg_info
-
-mkdir -p $BUILD_DIR/$NAME-$VERSION/data/pithos.egg-info
-cp data/$NAME.desktop $BUILD_DIR/$NAME-$VERSION/data
-cp data/$NAME $BUILD_DIR/$NAME-$VERSION/data
-cp -r pithos.egg-info/* $BUILD_DIR/$NAME-$VERSION/data/pithos.egg-info
-
-mkdir -p $BUILD_DIR/$NAME-$VERSION/pithos
-cp -r pithos/* $BUILD_DIR/$NAME-$VERSION/pithos
-find $BUILD_DIR/$NAME-$VERSION/pithos -name __pycache__ -exec rm -rf {} \;
-find $BUILD_DIR/$NAME-$VERSION/pithos -name *.pyc -exec rm -rf {} \;
-
-mkdir -p $BUILD_DIR/$NAME-$VERSION/data/icons/hicolor/scalable
-mkdir -p $BUILD_DIR/$NAME-$VERSION/data/icons/hicolor/48
-cp data/icons/hicolor/pithos.svg $BUILD_DIR/$NAME-$VERSION/data/icons/hicolor/scalable
-cp data/icons/hicolor/pithos-tray-icon.png $BUILD_DIR/$NAME-$VERSION/data/icons/hicolor/48
-
-mkdir -p $BUILD_DIR/$NAME-$VERSION/data/icons/ubuntu-mono-dark
-cp data/icons/ubuntu-mono-dark/*.svg $BUILD_DIR/$NAME-$VERSION/data/icons/ubuntu-mono-dark
-
-mkdir -p $BUILD_DIR/$NAME-$VERSION/data/icons/ubuntu-mono-light
-cp data/icons/ubuntu-mono-light/*.svg $BUILD_DIR/$NAME-$VERSION/data/icons/ubuntu-mono-light
+mkdir -p $BUILD_DIR/$NAME-$VERSION/debian/
 
 cd $BUILD_DIR
-tar -czf "${NAME}_${VERSION}.orig.tar.gz" $NAME-$VERSION
-cd ..
+wget "${SOURCE_URL_BASE}${VERSION}.tar.gz"
+tar -xf ${VERSION}.tar.gz
+mv ${VERSION}.tar.gz "${NAME}_${VERSION}.orig.tar.gz"
+cd $NAME-$VERSION
+cp -r ../../debian/* debian/
 
-mkdir -p $BUILD_DIR/$NAME-$VERSION/debian
-cp -r debian/* $BUILD_DIR/$NAME-$VERSION/debian
+pwd
+ls debian/changelog.in
 
-cd $BUILD_DIR/$NAME-$VERSION
+sed -i \
+    -e "s/@VERSION@/"${VERSION}"/g" \
+    -e "s/@DISTRO@/"${DISTRO}"/g" \
+    -e "s/@PKG_VERSION@/${PKG_VERSION}/g" \
+    -e "s/@DATE@/${DATE}/g" \
+    debian/changelog.in
+mv debian/changelog.in debian/changelog
+
 debuild -S -sa
 cd ..
-rm -rf $NAME-$VERSION
 
-echo "if build successful, run from the build directory: dput -f ppa:gsheremeta/pithos-staging ${NAME}_${VERSION}_source.changes"
+echo "run: cd build; dput -f ppa:gsheremeta/pithos-staging ${NAME}_${VERSION}_source.changes"
